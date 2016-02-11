e1f94645170756c87fa02fcaa5e79a72a39d28f9
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-11 13:27:32 -0500

    Create new employee and access their name and salary

--- employee.rb
+++ employee.rb
@@ -1,3 +1,15 @@
 class Employee
+  attr_reader :first_name, :last_name, :salary
 
+  def initialize(first_name, last_name, email, phone_number, salary)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @phone_number = phone_number
+    @salary = salary
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -10,6 +10,13 @@ class ArtistAndAlbumsTest < Minitest::Test
     assert Department
   end
 
-  
+  def test_create_new_employee_and_get_attributes
+    e = Employee.new("Brian", "Yarsawich", "test@test.com", "919-555-5555", 150000)
+    assert e
+    assert_equal "Brian", e.first_name
+    assert_equal "Yarsawich", e.last_name
+    assert_equal "Brian Yarsawich", e.name
+    assert_equal 150000, e.salary
+  end
 
 end
