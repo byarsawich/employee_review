4d01276fd419794249631280e1bb05bbed706d67
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-11 13:31:45 -0500

    Add and retrieve employee review.

--- employee.rb
+++ employee.rb
@@ -1,5 +1,6 @@
 class Employee
   attr_reader :first_name, :last_name, :salary
+  attr_accessor :review
 
   def initialize(first_name, last_name, email, phone_number, salary)
     @first_name = first_name
@@ -7,9 +8,11 @@ class Employee
     @email = email
     @phone_number = phone_number
     @salary = salary
+    @review = nil
   end
 
   def name
     "#{@first_name} #{@last_name}"
   end
+
 end
